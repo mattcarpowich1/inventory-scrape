5efde68299e6a9c260dc65de8344375ae9bd6af9
Matt Carpowich <mattcarpowich@gmail.com>
2022-02-20 21:17:04 -0800

    trying to get some linting

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,53 @@ import puppeteer from 'puppeteer';
 import Database from './db';
 import { InventoryItem } from './types';
 
-const URL = 'https://albumsurf.com';
+const URL = 'https://albumsurf.com/collections/new-boards';
 const INVENTORY_ITEM_SELECTOR = '.product-block';
+const PAGINATION_ELEMENT_SELECTOR = '.pagination__number'; 
 
 const db: Database = new Database();
 
 const init = async () => {
+  const browser = await puppeteer.launch();
   try {
-    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(URL);
+    await page.goto(URL, {waitUntil: 'load', timeout: 0});
 
-    // Get latest inventory
-    const latestInventory: InventoryItem[] = await page.$$eval(INVENTORY_ITEM_SELECTOR, items =>
-      items.map(item => {
-        const productId = item.getAttribute('data-product-id');
-        const title = item.querySelector('.product-block__title')?.textContent;
-        return {
-          productId,
-          title
-        };
-      })
-    );
+    const numberOfPagesInventory = await page.$$eval(PAGINATION_ELEMENT_SELECTOR, items => items.length);
 
-    latestInventory.forEach((item: InventoryItem) => {
-      const existsInDb = !!(db.select().byPKey(item.productId));
-      if (!existsInDb) {
-        db.upsert(item);
+    for (let inventoryPageNumber = 0; inventoryPageNumber < numberOfPagesInventory; inventoryPageNumber += 1) {
+      
+      // go to the right page
+      if (inventoryPageNumber > 0) {
+        await page.goto(`${URL}?page=${inventoryPageNumber + 1}`, {waitUntil: 'load', timeout: 0});
       }
-    });
 
-    await browser.close();
+      // Get the list of inventory items on each page
+      const latestInventory: InventoryItem[] = await page.$$eval(INVENTORY_ITEM_SELECTOR, items =>
+        items.map(item => {
+          const productId = item.getAttribute('data-product-id');
+          const title = item.querySelector('.product-block__title')?.textContent;
+          return {
+            productId,
+            title
+          };
+        })
+      );
+
+      // If the item is not in the db, then add it
+      latestInventory.forEach((item: InventoryItem) => {
+        const existsInDb = !!(db.select().byPKey(item.productId));
+        if (!existsInDb) {
+          db.upsert(item);
+        }
+      });
+    }
 
   } catch (e: unknown) {
-    console.log('Oh no!');
+    console.log('Oh no!', e);
   }
 
+  await browser.close();
 };
 
 init();
