f92f2d752bf89bd5a3927c7c9e726d8a9c88f1e2
Matt Carpowich <mattcarpowich@gmail.com>
2022-03-06 09:45:41 -0800

    started query boards by vendor product id

---
+++ src/getBoardByVendorProductId.ts
@@ -0,0 +1,28 @@
+import { gql } from 'graphql-request';
+import client from './graphClient';
+
+interface BoardQueryResponse {
+    boards: number[]
+}
+
+export const getBoardsByVendorProductIdQuery = gql`
+    query MyQuery($vendorId: Int!, $vendorProductId: String!) {
+        boards(where: {_and: {vendorId: {_eq: $vendorId}, vendorProductId: {_eq: $vendorProductId}}}) {
+            id
+        }
+}
+`;
+
+const getBoardsByVendorProductId = async (vendorId: number, vendorProductId: string): Promise<number[]> => {
+    try {
+        const data: BoardQueryResponse = await client.request(getBoardsByVendorProductIdQuery, {
+            vendorId,
+            vendorProductId,
+        });
+        return data.boards;
+    } catch (e: unknown) {
+        throw new Error(`Error querying boards by vendor product it: ${e as string}`);
+    }
+};
+
+export default getBoardsByVendorProductId;
